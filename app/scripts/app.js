e5aa9652d789d39e52638036c3be381f25c6c5f8
mizilin <mizi.lin@hunteron.com>
2014-08-06 12:08:57 +0800

    ng fw

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name cvofferApp
+ * @description
+ * # cvofferApp
+ *
+ * Main module of the application.
+ */
+angular.module('cvoffer', [
+    'cvoffer.controllers'
+]);
+
+angular.module('cvoffer.controllers', [
+    'cvoffer.controllers.default'
+]);
